dd187e3713dd1caf6f5c3da9abb75dbee63cd116
0pripyat92 <devjr0000@gmail.com>
2023-12-07 18:38:18 -0300

    commt geral

--- aula 012/ex 014/script.js
+++ aula 012/ex 014/script.js
@@ -2,8 +2,8 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    //var hora = data.getHours()
-    var hora = 19  
+    var hora = data.getHours()
+    //var hora = 19  
     msg.innerHTML = `Agora são ${hora} horas.` 
     if (hora >= 0 && hora < 12) { 
         //Bom dia!
---
+++ aula 014/ambiente.js
@@ -0,0 +1,9 @@
+/*for(var c = 1;c<=20;c++) {
+    console.log(c)
+}*/
+console.log('vai começar')
+var c = 1
+while (c<=5) {
+    console.log(c)
+    c +=1}
+    console.log('fim')
